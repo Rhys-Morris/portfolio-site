fbc76c33ce17f7177a835c7486c9fd30567e5c99
Rhys Morris <rhysmorris08@gmail.com>
2020-10-25 19:15:17 +1100

    Code refactor and redesign layout

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Reshow navbar on scroll up
+
+const nav = document.querySelector('.nav');
+
+window.addEventListener('scroll', (e) => {
+    console.log("The window has been scrolled");
+})
+
+// Hide scroll down indicator as soon as user scrolls down
+
+// Section content fade in with scroll down page
+
+// Navigate smoothly with navbar links
+
+// Form submission
